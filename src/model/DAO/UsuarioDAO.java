78101df1830fbd383a7b51d3469d7f47c3c9a2e5
alex-rangel <alexrangel90@gmail.com>
2024-09-10 00:13:46 -0300

    acertei a classe usuarioDAO

--- src/model/DAO/UsuarioDAO.java
+++ src/model/DAO/UsuarioDAO.java
@@ -76,37 +76,6 @@ public class UsuarioDAO {
 
 	}
 
-	/*
-	 * public static List<Cliente> buscarTodos() { List<Cliente> clientes = new
-	 * LinkedList<Cliente>();
-	 * 
-	 * Conexao conexao = new Conexao(configurador.getUrl(),
-	 * configurador.getDriver(), configurador.getLogin(), configurador.getSenha());
-	 * Connection con = conexao.obterConexao();
-	 * 
-	 * String sql = "select * from cliente";
-	 * 
-	 * try { Statement comando = con.createStatement();
-	 * 
-	 * ResultSet rs = comando.executeQuery(sql);
-	 * 
-	 * while (rs.next()) { Cliente cliente = new Cliente();
-	 * cliente.setId(rs.getInt("id")); cliente.setNome(rs.getString("nome"));
-	 * cliente.setCpf(rs.getString("cpf")); cliente.setEmail(rs.getString("email"));
-	 * 
-	 * clientes.add(cliente); }
-	 * 
-	 * rs.close(); comando.close(); con.close();
-	 * 
-	 * } catch (SQLException e) {
-	 * System.out.println("Erro ao buscar no Banco de Dados.");
-	 * System.out.println("Verifique sua instrução SQL.");
-	 * System.out.println("Mensagem de erro: " + e.getMessage());
-	 * e.printStackTrace(); }
-	 * 
-	 * return clientes; }
-	 */
-
 	public static boolean excluir(int id) {
 		boolean ok = false;
 
@@ -133,87 +102,5 @@ public class UsuarioDAO {
 		}
 		return ok;
 	}
-
-	/*
-	 * public static Cliente buscarPorId(int id) { Cliente cliente = null;
-	 * 
-	 * Conexao conexao = new Conexao(configurador.getUrl(),
-	 * configurador.getDriver(), configurador.getLogin(), configurador.getSenha());
-	 * Connection con = conexao.obterConexao();
-	 * 
-	 * String sql = "select * from cliente where id=?";
-	 * 
-	 * try { PreparedStatement comando = con.prepareStatement(sql);
-	 * comando.setInt(1, id);
-	 * 
-	 * ResultSet rs = comando.executeQuery();
-	 * 
-	 * if (rs.next()) { cliente = new Cliente(); cliente.setId(rs.getInt("id"));
-	 * cliente.setNome(rs.getString("nome")); cliente.setCpf(rs.getString("cpf"));
-	 * cliente.setEmail(rs.getString("email")); }
-	 * 
-	 * rs.close(); comando.close(); con.close();
-	 * 
-	 * } catch (SQLException e) {
-	 * System.out.println("Erro ao buscar por ID no Banco de Dados.");
-	 * System.out.println("Verifique sua instrução SQL.");
-	 * System.out.println("Mensagem de erro: " + e.getMessage());
-	 * e.printStackTrace(); } return cliente; }
-	 * 
-	 * public static boolean atualizar(int id, String nome, String cpf, String
-	 * email) { boolean ok = false;
-	 * 
-	 * Conexao conexao = new Conexao(configurador.getUrl(),
-	 * configurador.getDriver(), configurador.getLogin(), configurador.getSenha());
-	 * Connection con = conexao.obterConexao();
-	 * 
-	 * String sql = "update cliente set nome=?,cpf=?,email=? where id=?";
-	 * 
-	 * try { PreparedStatement comando = con.prepareStatement(sql);
-	 * comando.setString(1, nome); comando.setString(2, cpf); comando.setString(3,
-	 * email); comando.setInt(4, id);
-	 * 
-	 * ok = comando.executeUpdate() > 0;
-	 * 
-	 * comando.close(); con.close();
-	 * 
-	 * } catch (SQLException e) {
-	 * System.out.println("Erro ao atualizar no Banco de Dados.");
-	 * System.out.println("Verifique sua instrução SQL.");
-	 * System.out.println("Mensagem de erro: " + e.getMessage());
-	 * e.printStackTrace(); } return ok;
-	 * 
-	 * }
-	 * 
-	 * public static List<Cliente> buscarPorEmail(String email) { List<Cliente>
-	 * clientes = new LinkedList<Cliente>();
-	 * 
-	 * Conexao conexao = new Conexao(configurador.getUrl(),
-	 * configurador.getDriver(), configurador.getLogin(), configurador.getSenha());
-	 * Connection con = conexao.obterConexao();
-	 * 
-	 * String sql = "select * from cliente where email=?";
-	 * 
-	 * try { PreparedStatement comando = con.prepareStatement(sql);
-	 * comando.setString(1, email);
-	 * 
-	 * ResultSet rs = comando.executeQuery();
-	 * 
-	 * while (rs.next()) { Cliente cliente = new Cliente();
-	 * cliente.setId(rs.getInt("id")); cliente.setNome(rs.getString("nome"));
-	 * cliente.setCpf(rs.getString("cpf")); cliente.setEmail(rs.getString("email"));
-	 * 
-	 * clientes.add(cliente); }
-	 * 
-	 * rs.close(); comando.close(); con.close();
-	 * 
-	 * } catch (SQLException e) {
-	 * System.out.println("Erro ao buscar por e-mail no Banco de Dados.");
-	 * System.out.println("Verifique sua instrução SQL.");
-	 * System.out.println("Mensagem de erro: " + e.getMessage());
-	 * e.printStackTrace(); } return clientes;
-	 * 
-	 * }
-	 */
 
 }
